62e2ac0e8ba5e6004682934b4fef2bc929cd4185
Sushant Chavan <sushantchavan7143@gmail.com>
2026-07-27 21:22:09 +0530

    #

---
+++ AreaOfSquere/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AreaOfSquere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double s , l;
+            Console.Write("Enter the side length :");
+            s = Convert.ToDouble(Console.ReadLine());
+            l = s * s;
+            Console.WriteLine("Area of Triangle : " + l);
+            Console.ReadKey();
+        }
+    }
+}
